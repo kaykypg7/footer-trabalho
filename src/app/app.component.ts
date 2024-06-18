0e4a4dacaeb424b513543fcd6f64e887022e2138
Kayky <112106334+kaykypg7@users.noreply.github.com>
2024-06-18 12:59:32 -0300

    add main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RodapeComponent } from './rodape/rodape.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RodapeComponent],
+  imports: [RouterOutlet,RodapeComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
